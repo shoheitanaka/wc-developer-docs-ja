bd736f63400bcb4e3a6e9f67eeea02c8b047edb4
Shohei Tanaka <shohei.t@artws.info>
2022-02-06 19:00:16 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'wc_dev_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
